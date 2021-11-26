04d13e8818b447690ca4c94782d3316b7f20c70d
Guill Lo <guill.bout@gmail.com>
2021-11-26 23:22:04 +0800

    update connector for Monterey, remove all the crap from before

---
+++ src/connector.sh
@@ -0,0 +1,25 @@
+SYSTEM_PROFILER=$(system_profiler SPBluetoothDataType 2>/dev/null)
+
+MAC_ADDRESS=$(grep -B8 "Minor Type: Headphones" <<< "${SYSTEM_PROFILER}" | awk '/Address/{ print $2 }')
+CONNECTED=$(grep -A6 "${MAC_ADDRESS}" <<< "${SYSTEM_PROFILER}" | awk '/Connected: Yes/{ print 1 }')
+NAME=$(grep -B9 "Minor Type: Headphones" <<< "${SYSTEM_PROFILER}" | awk '/AirPods/{ print }' | sed -e 's/^ *//' -e 's/://')
+
+if [[ "${CONNECTED}" ]]; then
+  status="disconnect ${NAME}"
+  # disconnect doesn't work on Monterey if we don't force notify LMAO
+  arg="--disconnect ${MAC_ADDRESS//:/-} --notify"
+else
+  status="connect ${NAME}"
+  arg="--connect ${MAC_ADDRESS//:/-}"
+fi
+
+cat << EOB
+{"items": [
+    {
+        "uid": "connector",
+        "type": "default",
+        "title": "$status",
+        "arg": "$arg",
+    }
+]}
+EOB
