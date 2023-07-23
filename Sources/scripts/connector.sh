d87340bc5babd25e4dea18bd7602d18c30cdb2f0
G <guill.bout@gmail.com>
2023-07-23 14:39:26 +0800

    update script, differentiate Apple silicon from Intel

--- Sources/scripts/connector.sh
+++ Sources/scripts/connector.sh
@@ -1,13 +1,19 @@
 SYSTEM_PROFILER=$(system_profiler SPBluetoothDataType 2>/dev/null)
 
 MAC_ADDRESS=$(grep -B8 "Minor Type: Headphones" <<< "${SYSTEM_PROFILER}" | awk '/Address/{ lastline = $2 } END { print lastline }')
-CONNECTED=$(grep -A10 "${MAC_ADDRESS}" <<< "${SYSTEM_PROFILER}" | awk '/Services:/{print 1}')
+
+if [[ $(arch) == 'arm64' ]]; then
+  CONNECTED=$(resources/binaries/blueutil_arm64 --is-connected ${MAC_ADDRESS})
+else
+  CONNECTED=$(resources/binaries/blueutil_i386 --is-connected ${MAC_ADDRESS})
+fi
+
 NAME=$(grep -B9 "Minor Type: Headphones" <<< "${SYSTEM_PROFILER}" | awk '/AirPods/{ print }' | sed -e 's/^ *//' -e 's/://')
 
-if [[ "${CONNECTED}" ]]; then
+if [[ "${CONNECTED}" == 1 ]]; then
   status="disconnect ${NAME}"
   # disconnect doesn't work on Monterey if we don't force notify LMAO
-  arg="--disconnect ${MAC_ADDRESS//:/-} --notify"
+  arg="--disconnect ${MAC_ADDRESS//:/-}"
 else
   status="connect ${NAME}"
   arg="--connect ${MAC_ADDRESS//:/-}"
